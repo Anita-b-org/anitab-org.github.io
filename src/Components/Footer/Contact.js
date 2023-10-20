40f0ad0d338a62dc9350352ff6a23b11b1b76389
priya saw <priyasaw2452gmail.com>
2023-10-20 12:24:51 +0530

    footer UI enhancement

--- src/Components/Footer/Contact.js
+++ src/Components/Footer/Contact.js
@@ -11,13 +11,42 @@ function Contact() {
 
   return (
     <View style={[styles.col, styles.right]}>
-      <Text style={[styles.text, styles.heading]}>Contact Us</Text>
+      <Text
+        style={[
+          styles.text,
+          styles.heading,
+          { fontSize: 40, textAlign: 'justify', fontFamily: 'arial' },
+        ]}
+      >
+        Contact Us
+      </Text>
       {content.map((content, i) => (
         <Hyperlink
           linkStyle={{ color: '#2980b9' }}
           onPress={(url) => window.open(url, '_blank')}
         >
-          <Text style={[styles.text, styles.description]} key={i}>
+          <Text
+            className="pop"
+            style={[
+              styles.text,
+              styles.description,
+              {
+                center:
+                  content.par ===
+                  'Having a question in your mind or thinking about getting started with open-source? We are here to help!'
+                    ? 30
+                    : 20,
+                fontSize:
+                  content.par ===
+                  'Having a question in your mind or thinking about getting started with open-source? We are here to help!'
+                    ? 30
+                    : 20,
+                textAlign: 'justify',
+                fontFamily: 'DejaVu Serif',
+              },
+            ]}
+            key={i}
+          >
             {content.par}
           </Text>
         </Hyperlink>
